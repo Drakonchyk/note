e9ca72817fa4831de2a905db2ee18c8bde9a71a2
olenaazarova <azarova.pn@ucu.edu.ua>
2023-03-26 14:40:47 +0300

    search class

---
+++ backend/search.py
@@ -0,0 +1,51 @@
+""" Search class """
+from pymongo import MongoClient
+import json
+
+class Search:
+    def __init__(self, request, instruments = ['kalimba', 'guitar', 'ukulele', \
+'piano', 'drums'], tipe = 'both'):
+        self.instruments = instruments
+        self.request = request
+        self.tipe = tipe # 'chords', 'tabs' or 'both'
+        self.request = request
+
+    def find(self):
+        result = []
+        collections = []
+        if 'guitar' in self.instruments:
+            collections.append(db.Guitar)
+        if 'kalimba' in self.instruments:
+            collections.append(db.Kalimba)
+        if 'ukulele' in self.instruments:
+            collections.append(db.Ukulele)
+        if 'piano' in self.instruments:
+            collections.append(db.Piano)
+        if 'drums' in self.instruments:
+            collections.append(db.Drums)
+
+        for collection in collections:
+            if self.tipe == 'both':
+                file = list(collection.find({'title': self.request}))
+                file += (list(collection.find({'author': self.request})))
+            elif self.tipe == 'chords':
+                file = list(collection.find({'title': self.request, 'categories':'chords'}))
+                file += (list(collection.find({'author': self.request, 'categories':'chords'})))
+            else:
+                file = list(collection.find({'title': self.request, 'categories':'tabs'}))
+                file += (list(collection.find({'author': self.request, 'categories':'tabs'})))
+
+            for song in file:
+                if song not in result:
+                    result.append(song)
+        if result:
+            return result
+        return 'Nothing found'
+
+client = MongoClient("mongodb+srv://user:user-password@testcluster.tyin0tg.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.get_database('SongDatabase')
+
+
+i = Search("Stairway to Heaven", ['guitar']) # example
+print(i.find())
