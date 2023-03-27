a8d7834e2cd86f3bfe49dfe8791d784c6b946ff5
Drakonchyk <63148058+Drakonchyk@users.noreply.github.com>
2023-03-27 14:42:40 +0300

    remade some files

--- filters.py
+++ filters.py
@@ -1,57 +1,51 @@
-""" Search class """
 from pymongo import MongoClient
-import json
 
 client = MongoClient("mongodb+srv://user:user-password@testcluster.tyin0tg.mongodb.net/?retryWrites=true&w=majority")
 db = client.get_database('SongDatabase')
 
 class Filter:
-    def __init__(self, instruments = ['kalimba', 'guitar', 'ukulele', 'piano', 'drums'], \
-                 tipe = 'both') -> None:
+    def __init__(self, instruments=['kalimba', 'guitar', 'ukulele', 'piano', 'drums'], tipe='both'):
         self.instruments = instruments
-        self.tipe = tipe # 'chords', 'tabs' or 'both'
+        self.collections = []
+        self.tipe = tipe
 
-class Search (Filter):
-    def __init__(self, request, instruments = ['kalimba', 'guitar', 'ukulele', \
-'piano', 'drums'], tipe = 'both') -> None:
+    def get_filtered_songs(self):
+        if 'guitar' in self.instruments:
+            self.collections.append(db.Guitar)
+        if 'kalimba' in self.instruments:
+            self.collections.append(db.Kalimba)
+        if 'ukulele' in self.instruments:
+            self.collections.append(db.Ukulele)
+        if 'piano' in self.instruments:
+            self.collections.append(db.Piano)
+        if 'drums' in self.instruments:
+            self.collections.append(db.Drums)
+
+        return self.collections
+
+
+class Search(Filter):
+    def __init__(self, request = '', instruments=['kalimba', 'guitar', 'ukulele', 'piano', 'drums'], tipe='both'):
         super().__init__(instruments, tipe)
         self.request = request
 
     def find(self):
         result = []
-        collections = []
-        if 'guitar' in self.instruments:
-            collections.append(db.Guitar)
-        if 'kalimba' in self.instruments:
-            collections.append(db.Kalimba)
-        if 'ukulele' in self.instruments:
-            collections.append(db.Ukulele)
-        if 'piano' in self.instruments:
-            collections.append(db.Piano)
-        if 'drums' in self.instruments:
-            collections.append(db.Drums)
-
+        collections = self.get_filtered_songs()
         for collection in collections:
             if self.tipe == 'both':
-                file = list(collection.find({'title': self.request}))
-                file += (list(collection.find({'author': self.request})))
+                file = list(collection.find({'$or': [{'title': {'$regex': self.request}}, {'author': {'$regex': self.request}}]}))
             elif self.tipe == 'chords':
-                file = list(collection.find({'title': self.request, 'categories':'chords'}))
-                file += (list(collection.find({'author': self.request, 'categories':'chords'})))
+                file = list(collection.find({'$and': [{'categories': 'chords'}, {'$or': [{'title': {'$regex': self.request}}, {'author': {'$regex': self.request}}]}]}))
             else:
-                file = list(collection.find({'title': self.request, 'categories':'tabs'}))
-                file += (list(collection.find({'author': self.request, 'categories':'tabs'})))
+                file = list(collection.find({'$and': [{'categories': 'tabs'}, {'$or': [{'title': {'$regex': self.request}}, {'author': {'$regex': self.request}}]}]}))
 
             for song in file:
                 if song not in result:
                     result.append(song)
+
         if result:
             return result
-        return 'Nothing found'
-
-
-# i = Search("Stairway to Heaven", ['guitar']) # example
-# print(i.find())
-
-
+        else:
+            return 'Nothing found'
 
