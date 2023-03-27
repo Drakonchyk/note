fd1cddecb7bb93576f27cfd2a8fb28d8fabadfd3
olenaazarova <116667018+olenaazarova@users.noreply.github.com>
2023-03-27 14:46:33 +0300

    search returns all songs if there is no request

--- filters.py
+++ filters.py
@@ -1,15 +1,19 @@
+""" Search class """
 from pymongo import MongoClient
+import json
 
 client = MongoClient("mongodb+srv://user:user-password@testcluster.tyin0tg.mongodb.net/?retryWrites=true&w=majority")
 db = client.get_database('SongDatabase')
 
 class Filter:
-    def __init__(self, instruments=['kalimba', 'guitar', 'ukulele', 'piano', 'drums'], tipe='both'):
+    def __init__(self, instruments = ['kalimba', 'guitar', 'ukulele', 'piano', 'drums'], \
+                 tipe = 'both') -> None:
         self.instruments = instruments
         self.collections = []
-        self.tipe = tipe
+        self.tipe = tipe # 'chords', 'tabs' or 'both'
 
     def get_filtered_songs(self):
+
         if 'guitar' in self.instruments:
             self.collections.append(db.Guitar)
         if 'kalimba' in self.instruments:
@@ -23,29 +27,47 @@ class Filter:
 
         return self.collections
 
-
-class Search(Filter):
-    def __init__(self, request = '', instruments=['kalimba', 'guitar', 'ukulele', 'piano', 'drums'], tipe='both'):
+class Search (Filter):
+    def __init__(self, request="", instruments = ['kalimba', 'guitar', 'ukulele', 'piano', 'drums'], tipe = 'both') -> None:
         super().__init__(instruments, tipe)
         self.request = request
 
     def find(self):
         result = []
-        collections = self.get_filtered_songs()
+        collections = Filter(self.instruments).get_filtered_songs()
+        if self.request == "":
+            for collection in collections:
+                if self.tipe == 'both':
+                    file = list(collection.find({'$or': [{'categories': 'chords'}, {'categories': 'tabs'}]}))
+                elif self.tipe == 'chords':
+                    file = list(collection.find({'categories': 'chords'}))
+                else:
+                    file = list(collection.find({'categories': 'tabs'}))
+
+
+                for song in file:
+                    if song not in result:
+                        result.append(song)
+            return result
         for collection in collections:
             if self.tipe == 'both':
-                file = list(collection.find({'$or': [{'title': {'$regex': self.request}}, {'author': {'$regex': self.request}}]}))
+                file = list(collection.find({'title': self.request}))
+                file += (list(collection.find({'author': self.request})))
             elif self.tipe == 'chords':
-                file = list(collection.find({'$and': [{'categories': 'chords'}, {'$or': [{'title': {'$regex': self.request}}, {'author': {'$regex': self.request}}]}]}))
+                file = list(collection.find({'title': self.request, 'categories': 'chords'}))
+                file += (list(collection.find({'author': self.request, 'categories': 'chords'})))
             else:
-                file = list(collection.find({'$and': [{'categories': 'tabs'}, {'$or': [{'title': {'$regex': self.request}}, {'author': {'$regex': self.request}}]}]}))
+                file = list(collection.find({'title': self.request, 'categories': 'tabs'}))
+                file += (list(collection.find({'author': self.request, 'categories': 'tabs'})))
 
             for song in file:
                 if song not in result:
                     result.append(song)
-
         if result:
             return result
-        else:
-            return 'Nothing found'
+        return 'Nothing found'
 
+# j = Filter(['guitar'])
+# h = j.get_filtered_songs()
+# i = Search("Скрябін", ["guitar"], tipe='tabs') # example
+# print(i.find())
