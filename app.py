ee2bf00483372eabf734e99c39189c26ef856b99
Drakonchyk <63148058+Drakonchyk@users.noreply.github.com>
2023-03-28 22:47:29 +0300

    docstrings

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import secrets
+"""
+flask code for our 'note' project
+"""
+import secrets #for secret_key, generates secure random numbers for cryptographic purposes.
 from flask import Flask, render_template, request, abort
 import pymongo
 from bson import ObjectId
@@ -8,10 +11,13 @@ client = pymongo.MongoClient("mongodb+srv://user:user-password@testcluster.tyin0
 db = client.get_database('SongDatabase')
 
 app = Flask(__name__)
-app.secret_key = secrets.token_hex(16)
+app.secret_key = secrets.token_hex(16) #secret_key, should not change that
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
+    """
+    function which submits chosen filters to class Search
+    """
     if request.method == 'POST':
         query = request.form.get('query')
         if not query:
@@ -35,22 +41,30 @@ def search():
 
 @app.route('/')
 def welcome():
+    """
+    opens main page
+    """
     return render_template('welcome.html')
 
 @app.route('/about')
 def about():
+    """
+    opens page with information about the site
+    """
     return render_template('about.html')
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
+    """
+    function for adding songs in the database
+    all fields are required except for the checkbox 'I want_tabs!'
+    """
     if request.method == 'POST':
         name = request.form['song_name']
         authr = request.form['author_name']
         instrument = request.form['instr']
         text = request.form['song_text'].split('\n')
         choice = request.form.get('want_tabs')
-        if instrument is None or text is None or authr is None or name is None:
-            return 'You havent filled all fields!'
         if instrument == 'guitar':
             db.Guitar.insert_one({'title': name, 'author': authr,
                                   'categories': 'tabs' if choice else 'chords',
@@ -75,17 +89,18 @@ def create():
 
 @app.route('/object/<object_id>')
 def object_detail(object_id):
-    # Look up the object in the database using the object_id parameter
+    """
+    function which finds an object in the database and
+    renders a song page with its details
+    """
     obj = []
     for collection in Filter().get_filtered_songs():
         obj = list(collection.find({ '_id': ObjectId(object_id) }))
         if obj:
             break
-    # If the object is not found, return a 404 error
     if obj is None:
         abort(404)
     obj = obj[0]
-    # Render a template with the details of the object
     return render_template('song_pg.html', object=obj)
 
 if __name__ == '__main__':
